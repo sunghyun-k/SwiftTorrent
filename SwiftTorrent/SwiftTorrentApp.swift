5f31056d3e5eb3d9f68da970ff2bcfa34472e0cc
Sunghyun Kim <sunghyun_kim@jazzhallabong.com>
2022-04-06 22:35:58 +0900

    Initial Commit

---
+++ SwiftTorrent/SwiftTorrentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTorrentApp.swift
+//  SwiftTorrent
+//
+//  Created by Sunghyun Kim on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTorrentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
